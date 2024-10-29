fac8898748ede9d715c4beb93ff7d6c7cf27d182
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-10-29 13:22:33 -0700

    built in math functions

---
+++ basic_math.py
@@ -0,0 +1,41 @@
+import math
+
+# Basic operators
+# -----------------------
+friends = 0
+
+# friends = friends + 1
+# friends += 1
+# friends -= 1
+# friends *= 2
+# friends /= 2
+# friends **= 2
+# friends = friends % 2
+
+# print(friends)
+
+# Built in math functions
+# ----------------------
+
+x = 3.14
+y = -4
+z = 5
+
+round(x)  # returns 3
+abs(y)  # returns 4 - distance from 0
+pow(4, 3)  # returns 64 - 4 x 4 x 4 or 4 to power of 3
+max(x, y, z)  # returns maximum value of given numbers - 5
+min(x, y, z)  # returns minimum value of given numbers - -4
+
+# Math Module
+# import math - at top level of file
+# ----------------------
+
+math.pi
+math.e
+math.sqrt(x)  # find square root
+math.ceil(y)  # round up
+math.floor(y)  # round down
+
+# If statements
+# ----------------------
