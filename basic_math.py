17d42c2c1d8b2f61addc539ba46a756888142a13
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-10-30 14:14:50 -0700

    username validation

--- basic_math.py
+++ basic_math.py
@@ -80,14 +80,48 @@ math.floor(y)  # round down
 # String Methods
 # -----------------------
 
-string_name = input("Enter your full name: ")
+# string_name = input("Enter your full name: ")
 
-len(string_name)  # returns length of the string
-string_name.find(" ")  # finds the first occurance and returns its position value
-string_name.rfind(" ")  # finds the last occurance and returns its position value
-string_name.capitalize()  # makes the first letter a capital
-string_name.upper()  # Makes all letters uppercase
-string_name.isdigit()  # checks if all string items are numbers
-string_name.isalpha()  # checks for all alphabetical letters in a string
-string_name.count()  # counts how many characters are in a string
-string_name.replace()  # replaces all first parameter with another in a string
+# len(string_name)  # returns length of the string
+# string_name.find(" ")  # finds the first occurance and returns its position value
+# string_name.rfind(" ")  # finds the last occurance and returns its position value
+# string_name.capitalize()  # makes the first letter a capital
+# string_name.upper()  # Makes all letters uppercase
+# string_name.isdigit()  # checks if all string items are numbers
+# string_name.isalpha()  # checks for all alphabetical letters in a string
+# string_name.count()  # counts how many characters are in a string
+# string_name.replace()  # replaces all first parameter with another in a string
+
+# Validate user input
+# 1. username must not be more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
+# ------------------------------
+
+username_input = input("Username: ")
+
+if len(username_input) > 12:
+    print("Username is too long, must be less than 12 characters")
+    quit()
+elif not username_input.find(" "):
+    print("Username must not contain spaces or white space")
+    quit()
+elif not username_input.isalpha():
+    print("Username must not contain numbers or special characters")
+    quit()
+else:
+    print("Username accepted")
+
+# Quick function practice
+# def penny_rich():
+#     print(
+#         "Calculating if you got a penny a day and doubled it how much you would have in a month"
+#     )
+#     total = 0
+#     for x in range(1, 31):
+#         total += 0.01
+#         total *= 2
+#         print(f"on day {x} the total is {round(total, 2)}")
+
+
+# penny_rich()
