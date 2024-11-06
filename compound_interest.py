5e8bdc9c5675b346978aa2939066f26cc85e198e
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-11-06 13:01:49 -0700

    interest

--- compound_interest.py
+++ compound_interest.py
@@ -6,3 +6,15 @@ time = 0
 
 while principle <= 0:
     principle = float(input("Enter the principle amount: "))
+    if principle <= 0:
+        print("Principle can't be less than or equal to 0")
+
+while rate <= 0:
+    rate = float(input("Enter the interest rate: "))
+    if rate <= 0:
+        print("Rate can't be less than or equal to 0")
+
+while time <= 0:
+    time = float(input("Enter the time in years: "))
+    if time <= 0:
+        print("time can't be less than or equal to 0")
