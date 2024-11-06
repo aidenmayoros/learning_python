1a15fc9700fc0e6d838bb62c08533a0b09be170a
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-11-06 13:25:25 -0700

    loops done

--- for_loops.py
+++ for_loops.py
@@ -1,5 +1,12 @@
 # for loops execute a block of code a set number of times.
 # Can itterate over range, string, sequence ect.
 
-for x in range(0, 10):
-    print(x)
+# for x in range(1, 11):
+#     print(x)
+
+for x in range(1, 21):
+    if x == 13:
+        continue  # skip this itteration and go to the next
+        # break will stop the loop completly
+    else:
+        print(x)
