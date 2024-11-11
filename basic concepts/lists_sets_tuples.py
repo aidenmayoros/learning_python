bdf311fe5f4a0f1310167beabf9fa9c6756e2bd5
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-11-11 10:15:53 -0700

    start of collections

---
+++ basic concepts/lists_sets_tuples.py
@@ -0,0 +1,5 @@
+# Collection = single variable used to store multiple values.
+
+# List = [] ordered and changable. Duplicates OK
+# Set = {} unordered and immutable, but Add/Remove OK. NO duplicates
+# Tuple = () ordered and unchangable. Duplicates OK FASTER
