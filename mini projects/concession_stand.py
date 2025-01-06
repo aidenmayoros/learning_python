bc0c1753643271b18d33e949b194150dfb867a49
Aiden Mayoros <aiden.mayoros@gmail.com>
2025-01-05 21:49:04 -0700

    working on consession stand

--- mini projects/concession_stand.py
+++ mini projects/concession_stand.py
@@ -1,5 +1,19 @@
 # Concession stand program for practce of dictionary
 
+# Dictionary {key: value}
+
 menu = {
     "pizza": 3.00,
+    "nachos": 4.50,
+    "popcorn": 1.00,
+    "fries": 3.00,
+    "chips": 2.00,
+    "pretzel": 1.50,
+    "soda": 2.50,
 }
+
+cart = []
+total = 0
+
+for key, value in menu.items():
+    print(f"{key:10} ${value:.2f}")
