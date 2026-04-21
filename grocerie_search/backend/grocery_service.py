bcc814b0d4f6ede1eeb03af1dfd43e28d6d47a98
Aiden Mayoros <aiden.mayoros@gmail.com>
2026-04-21 14:33:12 -0400

    started adding fake data

--- grocerie_search/backend/grocery_service.py
+++ grocerie_search/backend/grocery_service.py
@@ -1,2 +1,24 @@
 """Grocery search and price comparison logic will go here."""
 
+# Fake Database data
+data = {
+    "item": "milk",
+    "cheapest_store": "Aldi",
+    "prices": {
+        "Aldi": 3.75,
+        "Walmart": 4.12,
+        "Commissary": 3.89,
+    },
+}
+
+
+def search_item(item_name):
+    return {
+        "item": item_name,
+        "cheapest_store": "Aldi",
+        "prices": {
+            "Aldi": 3.75,
+            "Walmart": 4.12,
+            "Commisary": 3.89,
+        },
+    }
--- grocerie_search/main.py
+++ grocerie_search/main.py
@@ -1,10 +1,6 @@
-"""Starting point for the grocery search app."""
-
-
 def main() -> None:
     pass
 
 
 if __name__ == "__main__":
     main()
-
